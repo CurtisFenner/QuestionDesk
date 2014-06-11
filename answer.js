7a8cef3cc40e98f6472a9853bf3add2f4c2aa08f
CurtisFenner <cfenneremail@gmail.com>
2014-06-11 18:34:18 -0400

    Aesthetic + Minor Link Improvements

--- answer.js
+++ answer.js
@@ -3,9 +3,9 @@ function Answer(fragment) {
 	// First; reorder the fragment into a list of objects and their modifiers
 	var link = [];
 	Links(fragment,link);
-	console.log(link);
+
 	for (var i = 0; i < link.length; i++) {
-		res.push(link[i][0].plain + " &rarr;" + link[i][1].plain);
+		res.push(link[i][0].plain + " &rarr; " + link[i][1].plain);
 	}
 	return "Find object restricted by:<ul><li>" + res.join("<li>") + "</ul>";
 }
@@ -24,6 +24,8 @@ function Links(fragment,modout) {
 		["prep-phrase","uncount"],
 		["obj","cop"],
 		["cop","sub"],
+		["sub","verb"],
+		["verb","sub"],
 		["obj","verb"],
 		["aux","cop"],
 		["aux","verb"],
@@ -34,19 +36,23 @@ function Links(fragment,modout) {
 		["qword","uncount"],
 		["det","noun"],
 		["det","uncount"],
-		["det","plural"]
+		["det","plural"],
+		["qword","cop"],
+		["obj","cop"]
 	];
 	var c = fragment.component;
 	if (c instanceof Array) {
 		for (var i = 0; i < c.length; i++) {
 			for (var j = 0; j < c.length; j++) {
-				var a = c[i].type;
-				var b = c[j].type;
-				for (var k = 0; k < mods.length; k++) {
-					var u = mods[k][0];
-					var v = mods[k][1];
-					if (starContain(a,u) && starContain(b,v)) {
-						modout.push([c[i],c[j]]);
+				if (i != j) {
+					var a = c[i].type;
+					var b = c[j].type;
+					for (var k = 0; k < mods.length; k++) {
+						var u = mods[k][0];
+						var v = mods[k][1];
+						if (starContain(a,u) && starContain(b,v)) {
+							modout.push([c[i],c[j]]);
+						}
 					}
 				}
 			}
